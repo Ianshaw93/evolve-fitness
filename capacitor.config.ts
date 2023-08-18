8560c1287b12df86520c3d0fa5263867e2fa4c5d
Ian <ianshaw93@gmail.com>
2023-08-18 08:45:19 +0100

    capacitor starter project

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'evolve-fitness',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+    url: "http://192.168.0.14:3000",
+    cleartext: true
+    
+  }
+};
+
+export default config;
